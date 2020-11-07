0f617de0204833ad42214cfb336b8aa294249323
actions <gitactions@gmail.com>
2020-11-07 21:05:14 +0000

    release

---
+++ backend/fe/precache-manifest.ba8e9734ba3d2d23f40ce6cd2a1fab5a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd5d01e8c02fc327655fb7ba6774d7c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b03e9a6950440bf9383f",
+    "url": "/static/css/main.ea001142.chunk.css"
+  },
+  {
+    "revision": "8224061280f15b19b930",
+    "url": "/static/js/2.6581e2ea.chunk.js"
+  },
+  {
+    "revision": "d6c34c6836fa137e0e44daca715e463c",
+    "url": "/static/js/2.6581e2ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b03e9a6950440bf9383f",
+    "url": "/static/js/main.2b5f8dd8.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "1bad09c155b7289ccaa7f4d404038663",
+    "url": "/static/media/google-login.1bad09c1.svg"
+  },
+  {
+    "revision": "41fa017a359322d955625fb5f97d8f1b",
+    "url": "/static/media/logo-login.41fa017a.svg"
+  }
+]);
