cf2f90beede0e2b69e0a448ab19b6889fca68f74
Oleksandr Starov <oleksandr.starov@leovegas.com>
2020-11-07 22:28:49 +0200

    Config for deployment

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,6 @@
 import bodyParser from 'body-parser';
 import express from 'express';
+const path = require('path');
 import { PORT } from './constant';
 import routes from './controller/index';
 import './data/db/mongoose';
@@ -13,6 +14,10 @@ app.use(bodyParser.json());
 app.use('/api/', routes);
 
 app.get('/healthcheck', (req, res) => res.send('Express + TypeScript Server'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './fe/index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
